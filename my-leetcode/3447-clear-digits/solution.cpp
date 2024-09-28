e5314c77ea2f77fdbe0192991d6ba18f249abbea
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2024-09-28 11:35:41 +0000

    Sync LeetCode submission Runtime - 2 ms (17.80%), Memory - 8 MB (100.00%)

---
+++ my-leetcode/3447-clear-digits/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans = "";
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='a' && s[i]<='z'){
+                ans.push_back(s[i]);
+            }
+            else{
+                ans.pop_back();
+            }
+        }
+        return ans;
+    }
+};
