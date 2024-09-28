fff12686f4386a03c1a53956a2204cf0f80efc7e
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2024-09-28 11:08:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.6 MB (71.43%)

---
+++ my-leetcode/2800-minimum-string-length-after-removing-substrings/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+           if(s[i]=='B' && st.size()>0 && st.top()=='A'){
+            st.pop();
+           }
+           else if(s[i]=='D' && st.size()>0 && st.top()=='C'){
+            st.pop();
+           }
+           else{
+            st.push(s[i]);
+           }
+    }
+    return st.size();
+    }
+};
