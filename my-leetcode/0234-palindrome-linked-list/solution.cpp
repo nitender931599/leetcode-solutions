8d4a42e7e682fb2ce2851c34560b7e167762c4b8
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-06-06 12:18:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 118.1 MB (53.49%)

--- my-leetcode/0234-palindrome-linked-list/solution.cpp
+++ my-leetcode/0234-palindrome-linked-list/solution.cpp
@@ -1,41 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution
-{
+class Solution {
 public:
-    bool isPalindrome(ListNode *head)
-    {
-
-        if (head == NULL)
-            return true;
-
-        ListNode *Temp = head;
-
-        vector<int> Nodes;
-
-        while (Temp != NULL)
-        {
-            Nodes.push_back(Temp->val);
-            Temp = Temp->next;
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* Next = NULL;
+        while(curr!=NULL){
+            Next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = Next;
         }
-
-        int st = 0;
-        int en = Nodes.size() - 1;
-
-        while (st <= en)
-        {
-            if (Nodes[st++] != Nodes[en--])
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newhead = reverseList(slow->next);
+        ListNode* a = head;
+        ListNode* b = newhead;
+        while(b!=NULL){
+            if(a->val != b->val){
                 return false;
+            }
+            a = a->next;
+            b = b->next;
         }
-
         return true;
     }
 };
