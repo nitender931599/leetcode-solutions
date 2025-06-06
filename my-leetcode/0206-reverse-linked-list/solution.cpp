127a148578422d8968bbd982aa83171299d6ea3f
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-06-06 11:06:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (71.79%)

--- my-leetcode/0206-reverse-linked-list/solution.cpp
+++ my-leetcode/0206-reverse-linked-list/solution.cpp
@@ -1,35 +1,15 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode *prev = NULL;
-        ListNode *temp = head;
-        ListNode *forw = NULL;
-
-        if(head == NULL)
-        {
-            return head;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* Next = NULL;
+        while(curr!=NULL){
+            Next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = Next;
         }
-        else
-        {
-             while(temp!=NULL)
-        {
-            forw = temp->next;
-            temp->next = prev;
-            prev = temp;
-            temp = forw;
-        }
-        
+        return prev;
     }
-    return prev;
-     } 
 };
