591f01e29172f5c43336e9e2afb9ed53d3d1b129
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-06-07 13:22:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (26.61%)

--- my-leetcode/0021-merge-two-sorted-lists/solution.cpp
+++ my-leetcode/0021-merge-two-sorted-lists/solution.cpp
@@ -1,26 +1,98 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        ListNode* tempA = a;
+        ListNode* tempB = b;
         ListNode* c = new ListNode(100);
-        ListNode* temp = c;
-        while(a!=NULL && b!=NULL){
-            if(a->val<=b->val){
-                temp->next = a;
-                a = a->next;
-                temp = temp->next;
+        ListNode* tempC = c;
+        while(tempA && tempB){
+            if(tempA->val<tempB->val){
+                tempC->next = tempA;
+                tempA = tempA->next;
+                tempC = tempC->next;
             }
             else{
-                temp->next = b;
-                b = b->next;
-                temp = temp->next;
+                tempC->next = tempB;
+                tempB = tempB->next;
+                tempC = tempC->next;
             }
         }
-        if(a==NULL){
-            temp->next = b;
+        if(tempA==NULL){
+            tempC->next = tempB;
         }
         else{
-            temp->next = a;
+            tempC->next = tempA;
         }
         return c->next;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ListNode* c = new ListNode(100);
+        // ListNode* temp = c;
+        // while(a!=NULL && b!=NULL){
+        //     if(a->val<=b->val){
+        //         temp->next = a;
+        //         a = a->next;
+        //         temp = temp->next;
+        //     }
+        //     else{
+        //         temp->next = b;
+        //         b = b->next;
+        //         temp = temp->next;
+        //     }
+        // }
+        // if(a==NULL){
+        //     temp->next = b;
+        // }
+        // else{
+        //     temp->next = a;
+        // }
+        // return c->next;
     }
 };
