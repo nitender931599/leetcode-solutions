79a70227d4c8505dc6cd14a20a8b9ab779b76e13
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-06-07 14:06:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.6 MB (61.90%)

--- my-leetcode/0328-odd-even-linked-list/solution.cpp
+++ my-leetcode/0328-odd-even-linked-list/solution.cpp
@@ -1,37 +1,86 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
         int count = 0;
+        ListNode* a = new ListNode(100);
+        ListNode* b = new ListNode(234);
+        ListNode* tempa = a;
+        ListNode* tempb = b;
         ListNode* temp = head;
-        ListNode* odd = new ListNode(100);
-        ListNode* even = new ListNode(100);
-        ListNode* temp1 = odd;
-        ListNode* temp2 = even;
         while(temp!=NULL){
             count++;
             if(count%2!=0){
-                temp1->next = temp;
+                tempa->next = temp;
+                tempa = tempa->next;
                 temp = temp->next;
-                temp1 = temp1->next;
             }
             else{
-                temp2->next = temp;
+                tempb->next = temp;
+                tempb = tempb->next;
                 temp = temp->next;
-                temp2 = temp2->next;
             }
         }
-        temp2->next = NULL;
-        temp1->next = even->next;
-        return odd->next;
+        tempb->next = NULL;
+        tempa->next = b->next;
+       // tempb->next = NULL;
+        return a->next;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     int count = 0;
+    //     ListNode* temp = head;
+    //     ListNode* odd = new ListNode(100);
+    //     ListNode* even = new ListNode(100);
+    //     ListNode* temp1 = odd;
+    //     ListNode* temp2 = even;
+    //     while(temp!=NULL){
+    //         count++;
+    //         if(count%2!=0){
+    //             temp1->next = temp;
+    //             temp = temp->next;
+    //             temp1 = temp1->next;
+    //         }
+    //         else{
+    //             temp2->next = temp;
+    //             temp = temp->next;
+    //             temp2 = temp2->next;
+    //         }
+    //     }
+    //     temp2->next = NULL;
+    //     temp1->next = even->next;
+    //     return odd->next;
+    // }
 };
