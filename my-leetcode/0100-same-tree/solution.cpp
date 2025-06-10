57a4c8d34d48036b45c1515d6f1dc46c17b02368
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-06-10 09:14:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.9 MB (12.96%)

--- my-leetcode/0100-same-tree/solution.cpp
+++ my-leetcode/0100-same-tree/solution.cpp
@@ -1,3 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
@@ -7,6 +18,6 @@ public:
         if(p == NULL || q == NULL){
             return false;
         }
-        return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
