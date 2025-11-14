0f82fbe470508fa7f0b21c48189a737e3e180131
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-11-14 09:27:25 +0000

    Sync LeetCode submission Runtime - 7 ms (42.19%), Memory - 26.4 MB (27.52%)

--- my-leetcode/0229-majority-element-ii/solution.cpp
+++ my-leetcode/0229-majority-element-ii/solution.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        vector<int>d;
-        map<int, int>m;
+        vector<int>ans;
+        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-            m[nums[i]]++;
+            mp[nums[i]]++;
         }
-        for(auto g : m){
-            if(g.second>n/3){
-                d.push_back(g.first);
+        for(auto it : mp){
+            if(it.second>n/3){
+                ans.push_back(it.first);
             }
         }
-        return d;
+        return ans;
     }
 };
