65a529155d52186a8c2edca826b7fb0db7adaef2
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-11-21 09:45:45 +0000

    Sync LeetCode submission Runtime - 12 ms (42.92%), Memory - 11.9 MB (52.79%)

--- my-leetcode/0141-linked-list-cycle/solution.cpp
+++ my-leetcode/0141-linked-list-cycle/solution.cpp
@@ -9,67 +9,16 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL || head->next == NULL){
-            return false;
-        }
         ListNode* slow = head;
         ListNode* fast = head;
+        ListNode* temp = head;
         while(fast!=NULL && fast->next!=NULL){
             slow = slow->next;
             fast = fast->next->next;
-            if(fast==slow){
+            if(slow==fast){
                 return true;
             }
         }
         return false;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // ListNode* slow = head;
-        // ListNode* fast = head;
-        // while(fast!=NULL && fast->next!=NULL){
-        //     slow = slow->next;
-        //     fast = fast->next->next;
-        //     if(slow == fast){
-        //         return true;
-        //     }
-        // }
-        // return false;
     }
 };
