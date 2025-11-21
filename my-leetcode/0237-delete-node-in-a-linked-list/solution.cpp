9f0d183fd542842862d9019a569bea52b27970d6
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-11-21 08:29:56 +0000

    Sync LeetCode submission Runtime - 10 ms (37.05%), Memory - 12.5 MB (21.67%)

--- my-leetcode/0237-delete-node-in-a-linked-list/solution.cpp
+++ my-leetcode/0237-delete-node-in-a-linked-list/solution.cpp
@@ -1,7 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    void deleteNode(ListNode* tar) {
-        tar->val = tar->next->val;
-        tar->next = tar->next->next;
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;       
+        node->next = node->next->next;
     }
 };
