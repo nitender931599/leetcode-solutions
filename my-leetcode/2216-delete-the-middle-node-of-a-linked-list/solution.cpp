fe5b946cf9a6ab7d200a42baf0f8bea3b6b520d4
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-11-24 20:05:35 +0000

    Sync LeetCode submission Runtime - 4 ms (34.82%), Memory - 312.1 MB (53.65%)

--- my-leetcode/2216-delete-the-middle-node-of-a-linked-list/solution.cpp
+++ my-leetcode/2216-delete-the-middle-node-of-a-linked-list/solution.cpp
@@ -11,70 +11,20 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if(head == NULL || head->next == NULL){
+        if(head == NULL || head->next==NULL){
             return NULL;
         }
+        ListNode* prev = NULL;
         ListNode* slow = head;
         ListNode* fast = head;
-        ListNode* prev = head;
         while(fast!=NULL && fast->next!=NULL){
             prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
-        prev->next = slow->next;
+        ListNode* d = prev->next;
+        prev->next = d->next;
+        delete d;
         return head;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // if(head == NULL || head->next == NULL){
-        //     return NULL;
-        // }
-        // ListNode* slow = head;
-        // ListNode* fast = head;
-        // ListNode* p = NULL;
-        // while(fast!=NULL && fast->next!=NULL){
-        //     p = slow;
-        //     slow = slow->next;
-        //     fast = fast->next->next;
-        // }
-        // p->next = slow->next;
-        // delete slow;
-        // return head;
-
     }
 };
