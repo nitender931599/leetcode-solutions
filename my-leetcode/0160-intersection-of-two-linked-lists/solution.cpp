3c6de927d810a80646c8b8745c9146b6fb975c0a
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-11-25 11:00:33 +0000

    Sync LeetCode submission Runtime - 41 ms (99.17%), Memory - 18.4 MB (99.69%)

--- my-leetcode/0160-intersection-of-two-linked-lists/solution.cpp
+++ my-leetcode/0160-intersection-of-two-linked-lists/solution.cpp
@@ -1,51 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode* tempA = headA;
         ListNode* tempB = headB;
-        int lenA = 0;
-        int lenB = 0;
-        while(tempA!=NULL){
-            lenA++;
-            tempA = tempA->next;
-        }
-        while(tempB!=NULL){
-            lenB++;
-            tempB = tempB->next;
-        }
-        tempA = headA;
-        tempB = headB;
-        if(lenA>lenB){
-        int diff = lenA - lenB;
-        for(int i=1;i<=diff;i++){
-            tempA = tempA->next;
+        if(headA==NULL || headB==NULL){
+            return NULL;
         }
         while(tempA!=tempB){
             tempA = tempA->next;
             tempB = tempB->next;
-        }
-        return tempA;
-        }
-        else{
-            int diff = lenB - lenA;
-            for(int i=1;i<=diff;i++){
-                tempB = tempB->next;
+            if(tempA == tempB){
+                return tempA;
             }
-            while(tempA!=tempB){
-            tempA = tempA->next;
-            tempB = tempB->next;
+            if(tempA==NULL){
+                tempA = headB;
+            }
+            if(tempB==NULL){
+                tempB = headA;
+            }
         }
         return tempA;
-        }
-
-    
     }
 };
