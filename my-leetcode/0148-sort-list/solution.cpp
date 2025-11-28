3619d7fc43de35b2bf23034a8303bea894bbcdb8
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-11-28 09:20:17 +0000

    Sync LeetCode submission Runtime - 59 ms (8.32%), Memory - 75.8 MB (6.30%)

--- my-leetcode/0148-sort-list/solution.cpp
+++ my-leetcode/0148-sort-list/solution.cpp
@@ -1,13 +1,83 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* merge(ListNode* list1, ListNode* list2){
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* temp = dummy;
+//         while(list1!=NULL && list2!=NULL){
+//             if(list1->val < list2->val){
+//                 temp->next = list1;
+//                 temp = list1;
+//                 list1 = list1->next;
+//             }
+//             else{
+//                 temp->next = list2;
+//                 temp = list2;
+//                 list2 = list2->next;
+//             }
+//         }
+//         if(list1!=NULL){
+//             temp->next = list1;
+//         }
+//         else{
+//             temp->next = list2;
+//         }
+//         return dummy->next;
+//     }
+
+//     ListNode* findmiddle(ListNode* head){
+//         ListNode* slow = head;
+//         ListNode* fast = head->next;
+//         while(fast!=NULL && fast->next!=NULL){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         return slow;
+//     }
+
+//     ListNode* sortList(ListNode* head) {
+//         if(head == NULL || head->next == NULL){
+//             return head;
+//         }
+//         ListNode* middle = findmiddle(head);
+//         ListNode* right = middle->next;
+//         middle->next = NULL;
+//         ListNode* left = head;
+
+//         left = sortList(left);
+//         right = sortList(right);
+//         return merge(left, right);
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     ListNode* merge(ListNode* list1, ListNode* list2){
@@ -28,7 +98,7 @@ public:
         if(list1!=NULL){
             temp->next = list1;
         }
-        else{
+        if(list2!=NULL){
             temp->next = list2;
         }
         return dummy->next;
@@ -52,7 +122,7 @@ public:
         ListNode* right = middle->next;
         middle->next = NULL;
         ListNode* left = head;
-
+        
         left = sortList(left);
         right = sortList(right);
         return merge(left, right);
