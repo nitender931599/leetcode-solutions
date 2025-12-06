17b4811c94b3fb4187dacf243fc4814a35371d6f
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2025-12-06 09:06:16 +0000

    Sync LeetCode submission Runtime - 4 ms (85.86%), Memory - 44.9 MB (47.43%)

--- my-leetcode/0455-assign-cookies/solution.cpp
+++ my-leetcode/0455-assign-cookies/solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int gg = g.size();
+        int gf = g.size();
         int ss = s.size();
-        sort(s.begin(), s.end());
+        int l = 0;
+        int r = 0;
         sort(g.begin(), g.end());
-        // int l=0,r = 0; // l-> cookie
-        int l,r;
-        l =r=0;
-        while(l<ss && r < gg){
-            if(g[r]<=s[l]){
-                cout<<"jj";
-                r=r+1;
+        sort(s.begin(), s.end());
+        while(l<ss && r<gf){
+            if(g[r] <= s[l]){
+                r = r+1;
             }
-            l=l+1;
+            l = l+1;
         }
         return r;
     }
