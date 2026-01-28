2f3012b19a06ca1fba2c7a5c846597ee669dbba0
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2026-01-28 11:41:35 +0000

    Sync LeetCode submission Runtime - 2 ms (96.56%), Memory - 22.8 MB (97.83%)

--- my-leetcode/0907-koko-eating-bananas/solution.cpp
+++ my-leetcode/0907-koko-eating-bananas/solution.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& v, int h) {
-        int start = 0, end = 0, ans, mid, n = v.size();
-    long long sum = 0;
-    for(int i=0;i<n;i++){
-        sum += v[i];
-        end = max(end, v[i]);
+
+    double calc(vector<int> &piles, int k){
+        long long sum = 0;
+        for(int i=0;i<piles.size();i++){
+            sum += (piles[i]+k-1)/k;
+        }
+        return sum;
     }
-    
-    start = sum/h;
-    if(!start)
-        start = 1;
-    while(start<=end){
-        mid = start+(end-start)/2;
-        int totaltime = 0;
-        for(int i=0;i<n;i++){
-            totaltime += v[i]/mid;
-            if(v[i]%mid){
-                totaltime++;
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+        while(left<=right){
+            int mid = left + (right - left) / 2;
+            long long totalh = calc(piles, mid);
+            if(totalh<=h){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
             }
         }
-        if(totaltime>h){
-            start = mid+1;
-        }
-        else{
-            ans = mid;
-            end = mid-1;
-        }
-    }
-    return ans;
+        return left;
     }
 };
