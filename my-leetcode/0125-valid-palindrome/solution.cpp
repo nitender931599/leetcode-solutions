20871fd74148bb4031287fefac9141b99aee7212
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2026-01-31 12:18:10 +0000

    Sync LeetCode submission Runtime - 1 ms (44.82%), Memory - 10.8 MB (5.14%)

--- my-leetcode/0125-valid-palindrome/solution.cpp
+++ my-leetcode/0125-valid-palindrome/solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string ans;
+        string ans = "";
         for(int i=0;i<s.length();i++){
             if(isalpha(s[i]) || isdigit(s[i])){
-                ans.push_back(tolower(s[i]));
+                ans+=tolower(s[i]);
             }
         }
-        string ss = ans;
+        string f = ans;
         reverse(ans.begin(), ans.end());
-        if(ss==ans){
+        if(ans==f){
             return true;
         }
         return false;
+        
     }
 };
