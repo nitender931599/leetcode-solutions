ae12233b7da59cf602fa8733613e390a208522df
Nitender Kumar <151771686+nitender931599@users.noreply.github.com>
2026-02-03 10:17:13 +0000

    Sync LeetCode submission Runtime - 3 ms (4.58%), Memory - 15.1 MB (68.79%)

--- my-leetcode/0898-transpose-matrix/solution.cpp
+++ my-leetcode/0898-transpose-matrix/solution.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> t(m, vector<int> (n));
+        vector<vector<int>>mat(m,vector<int>(n));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                t[j][i] = matrix[i][j];
+                mat[j][i] = matrix[i][j];
             }
         }
-        return t;
+        return mat;
     }
 };
